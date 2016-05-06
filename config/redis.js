ff80b769172e2f1106a9e9092db30c37f4f245a8
Eric Hacke <eric.hacke@groupbyinc.com>
2016-05-05 23:13:03 -0400

    Minor

--- config/redis.js
+++ config/redis.js
@@ -3,7 +3,7 @@ var Redis = require('ioredis');
 var redis = new Redis({
   port: 6379,
   host: process.env.REDIS_HOST || 'localhost',
-  db:   10 // So it can't possibly conflict with the recommendation cache
+  db:   10
 });
 
 export default redis;
