eb1cb1875e23dc1e1faed44687ed56d7f6827ff3
Eric Hacke <eric.hacke@groupbyinc.com>
2016-06-02 16:29:37 -0400

    Fix docker build add kubernetes

--- docker/index.js
+++ docker/index.js
@@ -1,4 +1,4 @@
 const Chillastic = require('chillastic');
 
-const chillastic = Chillastic(process.env.REDIS_HOST, process.env.REDIS_PORT);
+const chillastic = Chillastic(process.env.REDIS_HOST, parseInt(process.env.REDIS_PORT));
 chillastic.run();
