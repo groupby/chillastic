30a1fb7bd457eceed4aabc39629e64a5db64104d
Eric Hacke <eric.hacke@groupbyinc.com>
2016-06-07 16:09:29 -0400

    Starting

--- app/api/tasks/index.js
+++ app/api/tasks/index.js
@@ -6,7 +6,7 @@ router.get('/', controller.getTasks);
 router.post('/_start', controller.start);
 router.post('/_stop', controller.stop);
 router.post('/:id', controller.addTask);
-router.post('/:id', controller.deleteTask);
+router.delete('/:id', controller.deleteTask);
 
 module.exports = function () {
   return router;
--- app/api/tasks/tasks.controller.js
+++ app/api/tasks/tasks.controller.js
@@ -43,6 +43,52 @@ const getTasks = (req, res) => {
   }
 };
 
+const getTask = (req, res) => {
+
+  if (!Task.NAME_REGEX.test(req.params.id)) {
+    res.status(400).json({error: 'task name must have alphanumeric characters only'});
+    return;
+  }
+
+  const taskName = req.params.id;
+
+  services.manager.taskExists(taskName).then(exists => {
+    
+  });
+
+  try {
+    services.manager.getTasks().then(taskNames => {
+      return Promise.reduce(taskNames, (result, taskName) => {
+        const taskStatus = {};
+
+        return services.manager.getBacklogCount(taskName).then(count => {
+          taskStatus.backlog = count;
+          return services.manager.getCompletedCount(taskName);
+        }).then(count => {
+          taskStatus.completed = count;
+          return services.manager.getOverallProgress(taskName);
+        }).then(overallProgress => {
+          const inWork = _.reduce(overallProgress, (inner_result, progress) => {
+            inner_result += progress.progress.total;
+            return inner_result;
+          }, 0);
+
+          taskStatus.total           = taskStatus.backlog + taskStatus.completed + inWork;
+          taskStatus.percentComplete = ((taskStatus.completed / taskStatus.total) * 100).toFixed(2);
+          taskStatus.inProgress      = overallProgress;
+
+          result[taskName] = taskStatus;
+          return result;
+        });
+      }, {}).then(response => {
+        res.status(200).json(response);
+      }).catch(error => utils.processError(error, res));
+    });
+  } catch (error) {
+    utils.processError(error, res);
+  }
+};
+
 const addTask = (req, res)=> {
   if (!Task.NAME_REGEX.test(req.params.id)) {
     res.status(400).json({error: 'task name must have alphanumeric characters only'});
