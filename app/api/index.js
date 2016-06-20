d8aa82e040d971071b9947e510fe1a1e000b91ce
Osman Currim <osman.currim@groupbyinc.com>
2016-06-19 20:01:14 -0400

    move the middleware parsers into the app layer

--- app/api/index.js
+++ app/api/index.js
@@ -5,13 +5,14 @@ const tasks      = require('./tasks');
 const filters    = require('./filters');
 const mutators   = require('./mutators');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
 const router = express.Router();
 router.post('/_start', controller.start);
 router.post('/_stop', controller.stop);
 router.get('/status', controller.getStatus);
-
-router.use(bodyParser.json());
-router.use(bodyParser.text());
 
 router.use('/tasks', tasks());
 router.use('/filters', filters());
