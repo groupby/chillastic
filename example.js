eed81c0d352adf51e251a3bdbf73c6e6ececc298
Eric Hacke <eric.hacke@groupbyinc.com>
2016-06-20 18:30:35 -0400

    Random port

--- example.js
+++ example.js
@@ -1,8 +1,9 @@
+const _          = require('lodash');
 const Chillastic = require('./index'); // Replace with 'require('chillastic')' if you're outside of this repo
 
-const REDIS_HOST = 'redis';
+const REDIS_HOST = 'localhost';
 const REDIS_PORT = 6379;
-const CHILL_PORT = 8080;
+const CHILL_PORT = _.random(8000, );
 
 const chillastic = Chillastic(REDIS_HOST, REDIS_PORT, CHILL_PORT);
 
