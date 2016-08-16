afe2bd043e6325643c4b039e2d854a31342e69ce
Osman Currim <osman.currim@groupbyinc.com>
2016-08-16 16:57:04 -0400

    update cleanup script to deal with NaN

--- scripts/clear_completed_tasks.sh
+++ scripts/clear_completed_tasks.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-for task in $(curl -s chillastic.groupbycloud.com/tasks | jq 'to_entries[] | select(.value.percentComplete == "100.00") | .key'); do
+completed_key="${1:-"100.00"}"
+for task in $(curl -s chillastic.groupbycloud.com/tasks | jq 'to_entries[] | select(.value.percentComplete == "'"${completed_key}"'") | .key'); do
   id="$(echo ${task} | cut -c2-34)"
   curl -s "chillastic.groupbycloud.com/tasks/${id}" -XDELETE | jq .
 done
