522668a104d8d4d15fd5cb23d01eb29aa9fb0381
Osman Currim <osman.currim@groupbyinc.com>
2018-07-11 13:42:11 -0400

    update cleanup script

--- scripts/clear_completed_tasks.sh
+++ scripts/clear_completed_tasks.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 completed_key="${1:-"100.00"}"
-for task in $(curl -s chillastic.groupbycloud.com/tasks | jq 'to_entries[] | select(.value.percentComplete == "'"${completed_key}"'") | .key'); do
+for task in $(acurl -s chillastic.groupbycloud.com/tasks | jq 'to_entries[] | select(.value.percentComplete == "'"${completed_key}"'") | .key'); do
   id="$(echo ${task} | cut -c2-34)"
-  curl -s "chillastic.groupbycloud.com/tasks/${id}" -XDELETE | jq .
+  acurl -s "chillastic.groupbycloud.com/tasks/${id}" -XDELETE | jq .
 done
